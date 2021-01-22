ba0a314e7e5d1589a76b4e0d5d266d56941501b8
jj@k <jakam.jurgen@gmail.com>
2021-01-22 08:56:07 -0600

    modified the method to return int and take 3 variables

--- GreatestNum.java
+++ GreatestNum.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 public class GreatestNum {
 
     //This method ask user for 3 numbers and display the greatest of the 3 to the screen
-    public static void GreatestMethod() {
+    public static int GreatestMethod(int x, int y, int z) {
+        if (x > y) {
+            if (x > z) {
+                return x;
+            }
+            else {
+                return z;
+            }
+        }
+        else if (y > z) {
+            return y;
+        }
+        return z;
+    }
+
+    //Call the method
+    public static void main(String arg[]){
         System.out.println("please enter 3 number one after one (press enter after each number is entered)");
         Scanner scan = new Scanner(System.in);
         int num1 = scan.nextInt();
         int num2 = scan.nextInt();
         int num3 = scan.nextInt();     
         scan.close();
-        if (num1 > num2) {
-            if (num1 > num3) {
-                System.out.println("The greatest number is: "+num1);
-            }
 
-            else {
-                System.out.println("The greatest number is: "+num3);
-            }
-        }
-        else if (num2 > num3) {
-            System.out.println("The greatest number is: "+num2);
-        }
-    }
-
-    //Call the method
-    public static void main(String arg[]){
-        GreatestMethod();
+        System.out.println("The greatest number is: "+GreatestMethod(num1, num2, num3));
     }
 }
